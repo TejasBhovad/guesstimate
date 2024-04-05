5b4e5b64684daf7b40259936283de5a268d459be
TejasBhovad <tejasbhovad@gmail.com>
2024-04-05 19:35:13 +0530

    improved card

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -6,7 +6,10 @@ const Card = ({ position, selected, number }) => {
   return (
     <div className="flex flex-col absolute">
       <motion.div
-        className="w-72 h-96 bg-white rounded-lg shadow-lg p-2 card"
+        // set shadow-lg to card when selected and shadow-md when not selected
+        className={`w-72 h-96 bg-white rounded-lg p-2 card ${
+          selected ? "shadow-lg" : "shadow-md"
+        }`}
         initial={{ opacity: 0, x: 200, rotate: 30 }}
         animate={{
           opacity: 1,
