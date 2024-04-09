45e1f9da025f1469c64d3daa8f30e77bce8de387
Karan Singh <karans9609@gmail.com>
2024-04-09 19:07:06 +0530

    Updated Card

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({ position, selected, number }) => {
     <div className="flex flex-col absolute">
       <motion.div
         // set shadow-lg to card when selected and shadow-md when not selected
-        className={`w-72 h-96 bg-white rounded-lg p-2 card ${
+        className={`w-72 h-96 bg-white rounded-lg p-2 border-[12px] border-white card ${
           selected ? "shadow-lg" : "shadow-md"
         }`}
         initial={{ opacity: 0, x: 200, rotate: 30 }}
@@ -17,7 +17,7 @@ const Card = ({ position, selected, number }) => {
           y: position.y,
           rotate: position.rotate,
           //   make red is selected
-          backgroundColor: selected ? "#FDA4AF" : "white",
+          backgroundColor: selected ? "#FDA4AF" : "yellow",
         }}
         transition={{ type: "spring", stiffness: position.x === 0 ? 120 : 60 }} // adjust stiffness based on position.x
       >
