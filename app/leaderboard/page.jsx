71a111328d39f8a748c1df4e82716859bd6f1f8b
Karan Singh <karans9609@gmail.com>
2024-04-11 19:55:23 +0530

    Updated Data

--- app/leaderboard/page.jsx
+++ app/leaderboard/page.jsx
@@ -7,6 +7,11 @@ const leaderboard = () => {
     { name: 'User 3', score: 80 },
     { name: 'User 4', score: 70 },
     { name: 'User 5', score: 60 },
+    { name: 'User 6', score: 100 },
+    { name: 'User 7', score: 90 },
+    { name: 'User 8', score: 80 },
+    { name: 'User 9', score: 70 },
+    { name: 'User 10', score: 60 },
     // Add more data as needed
   ];
 
