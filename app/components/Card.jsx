15cfc93de91307a0b9107827125c12831596a164
Karan Singh <karans9609@gmail.com>
2024-04-11 18:57:27 +0530

    Improved Cards

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -1,13 +1,15 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
+import profileIcon from "./icons/Main.jpeg";
+import Image from "next/image";
 
 const Card = ({ position, selected, number, name, data }) => {
   return (
     <div className="flex flex-col absolute">
       <motion.div
         // set shadow-lg to card when selected and shadow-md when not selected
-        className={`w-72 h-96 bg-white rounded-xl p-2 card border-[12px] border-white flex flex-col ${
+        className={`w-72 h-96 bg-white rounded-xl card border-[12px] border-white flex flex-col ${
           selected ? "shadow-lg" : "shadow-md"
         }`}
         initial={{ opacity: 0, x: 200, y: -50, rotate: 30 }}
@@ -21,8 +23,15 @@ const Card = ({ position, selected, number, name, data }) => {
         }}
         transition={{ type: "spring", stiffness: position.x === 0 ? 120 : 60 }} // adjust stiffness based on position.x
       >
-        <span className="font-semibold">Card {number}</span>
-        <span className="font-semibold">{name}</span>
+        <Image src={profileIcon} className='-z-20 absolute w-full h-full object-cover' alt="" />
+        <div className="bg-white opacity-60">
+          {/* <span className="font-semibold">Card {number}</span> */}
+          <span className="font-bold border-b-2 border-gray-300">
+            <div className="ml-[5px] mr-[5px] mt-[5px] mb-[5px] text-center">
+              {name}
+            </div>
+          </span>
+        </div>
         {/* json with formatting */}
         <pre className="overflow-auto">{JSON.stringify(data, null, 2)}</pre>
       </motion.div>
