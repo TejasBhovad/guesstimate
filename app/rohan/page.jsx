72636e65be8a187c7e3d687d8f0d851d53b0f7e9
TejasBhovad <tejasbhovad@gmail.com>
2024-04-12 17:03:09 +0530

    corrected rohan

--- app/rohan/page.jsx
+++ app/rohan/page.jsx
@@ -5,14 +5,13 @@ import Link from "next/link";
 const page = () => {
   return (
     <div className="w-full flex justify-center mb-6">
-    <Link href="/leaderboard">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Go to Leaderboard
-      </button>
-    </Link>
-  </div>;
+      <Link href="/leaderboard">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Go to Leaderboard
+        </button>
+      </Link>
+    </div>
   );
 };
 
 export default page;
-
